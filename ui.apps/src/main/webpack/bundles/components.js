05e26d001bc0c577472c7284dccafd0d3526408a
kevinweber <k-iwi@web.de>
2017-07-10 17:52:27 -0700

    Add note about Babel Polyfill

--- ui.apps/src/main/webpack/bundles/components.js
+++ ui.apps/src/main/webpack/bundles/components.js
@@ -1,10 +1,17 @@
+/**
+ * Uncomment the following line to include Babel's polyfill.
+ * Note that this increases the size of the bundled JavaScript file.
+ * So be smart about when and where to include the polyfill.
+ */
+// import 'babel-polyfill';
+
 // https://webpack.js.org/guides/dependency-management/#require-context
 const cache = {};
 
 function importAll(r) {
-    r.keys().forEach((key) => {
-        return cache[key] = r(key);
-    });
+  r.keys().forEach((key) => {
+    return cache[key] = r(key);
+  });
 }
 
 // Include all files named "index.js" in a "webpack.modules/" folder.
