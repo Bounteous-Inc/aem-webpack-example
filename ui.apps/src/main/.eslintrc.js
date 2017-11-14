3d84806c60422cfa0398d2cbd16ae0b5eaf3c713
kevinweber <k-iwi@web.de>
2017-11-13 17:21:17 -0800

    Reduce rules

--- ui.apps/src/main/.eslintrc.js
+++ ui.apps/src/main/.eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
+  "extends": "eslint:recommended",
   "env": {
     "browser": true,
     "es6": true,
     "node": true
   },
-  "extends": "eslint:recommended",
   "globals": {
     "$": true,
     "Granite": true
   },
   "parserOptions": {
     "ecmaVersion": 8,
-    "sourceType": "module",
+    "sourceType": "module"
   },
   "rules": {
-    "no-console": "off",
-    "no-unused-vars": ["warn"],
-    "quotes": ["error", "single"],
-    "semi": 2
+    "no-console": "off"
   }
 };
