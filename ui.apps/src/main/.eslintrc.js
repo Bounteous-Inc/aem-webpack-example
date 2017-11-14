eb51047d1de1484bec398a21e60d6893acfd3603
kevinweber <k-iwi@web.de>
2017-11-13 17:37:21 -0800

    Mention `eslint-config-infield` in ESLint config

--- ui.apps/src/main/.eslintrc.js
+++ ui.apps/src/main/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   "extends": "eslint:recommended",
+  // Optional: Replace `eslint:recommended` with `eslint-config-infield` and run `npm install --save-dev eslint-config-infield eslint` for stricter linting rules
   "env": {
     "browser": true,
     "es6": true,
