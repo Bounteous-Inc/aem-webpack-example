2cf8e48fb7b560107504774cba1ae7ed7775d2f7
kevinweber <k-iwi@web.de>
2017-11-17 17:39:23 -0800

    Abstract out configuration options into separate config file

--- ui.apps/src/main/config/index.js
+++ ui.apps/src/main/config/index.js
@@ -1,36 +1,57 @@
+const path = require('path');
 const webpackConfig = require('../webpack/internals/webpack.config.js');
 
-module.exports = {
-  eslint: {
-    "extends": "eslint:recommended",
-    // Optional: Replace `eslint:recommended` with `eslint-config-infield` and run `npm install --save-dev eslint-config-infield eslint` for stricter linting rules
-    "env": {
-      "browser": true,
-      "es6": true,
-      "node": true
-    },
-    settings: {
-      // This prevents `no-unused-vars` and `import/no-unresolved` from being thrown
-      // when modules are imported that are defined in Webpack but not correctly
-      // resolved by `eslint-plugin-import`. Note that `eslint-plugin-import` can
-      // be defined directly in this project's package.json but it can also be an
-      // dependency of an extended ESLint configuration.
-      'import/resolver': {
-        node: {
-          paths: (webpackConfig.resolve && webpackConfig.resolve.modules) || [],
-        },
+const AEM = {
+  projectFolderName: '__appsFolderName__',
+  libraryName: 'Project_Component_Library', // This string must represent a valid JavaScript variable name (e.g., don't start with a number, and don't use spaces)
+  jcrRoot: path.resolve(__dirname, '../content/jcr_root/'),
+};
+
+/*
+ * Here we can add as many bundles files as we want. One entry results in one output file.
+ * <id>: Defines how the target file is named, e.g. 'main' results in 'main.bundle.js'.
+ * <path>: Defines the path to an entry file. Entry files can use require.context()
+ * to search a directory and include multiple files matching a pattern.
+ */
+AEM.entries = [{
+  id: 'components',
+  path: path.resolve(__dirname, './bundles/components.js')
+}];
+
+const ESLINT = {
+  "extends": "eslint:recommended",
+  // Optional: Replace `eslint:recommended` with `eslint-config-infield` and run `npm install --save-dev eslint-config-infield eslint` for stricter linting rules
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  settings: {
+    // This prevents `no-unused-vars` and `import/no-unresolved` from being thrown
+    // when modules are imported that are defined in Webpack but not correctly
+    // resolved by `eslint-plugin-import`. Note that `eslint-plugin-import` can
+    // be defined directly in this project's package.json but it can also be an
+    // dependency of an extended ESLint configuration.
+    'import/resolver': {
+      node: {
+        paths: (webpackConfig.resolve && webpackConfig.resolve.modules) || [],
       },
     },
-    "globals": {
-      "$": true,
-      "Granite": true
-    },
-    "parserOptions": {
-      "ecmaVersion": 8,
-      "sourceType": "module"
-    },
-    "rules": {
-      "no-console": "off"
-    }
+  },
+  "globals": {
+    "$": true,
+    "Granite": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-console": "off"
   }
+};
+
+module.exports = {
+  aem: AEM,
+  eslint: ESLINT,
 };
--- ui.apps/src/main/webpack/internals/webpack.config.js
+++ ui.apps/src/main/webpack/internals/webpack.config.js
@@ -1,35 +1,22 @@
 /**
- * Basic webpack configuration including Babel loader for preset 'latest'.
- *
- * Configuration for Webpack 2, see https://webpack.js.org/configuration/
+ * Basic configuration for Webpack 2, see https://webpack.js.org/configuration/.
+ * Don't touch is file you want to stick with the standard.
  */
 
 const webpack = require('webpack');
 const path = require('path');
 
+const CONFIG = require(path.resolve(__dirname, '../../config'));
+
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractCSS = new ExtractTextPlugin('[name].bundle.css');
 const NODE_MODULES = path.join(__dirname, '../node_modules');
 
-const libraryName = 'Project_Component_Library'; // This string must represent a valid JavaScript variable name (e.g., don't start with a number, and don't use spaces)
-const outputPath = path.resolve(__dirname, '../../content/jcr_root/apps/__appsFolderName__/clientlibs/webpack.bundles');
-
 const IS_PROD = (process.env.NODE_ENV === 'production');
-
-/*
- * Here we can add as many entry files as we want. One entry results in one output file.
- * <id>: Defines how the target file is named, e.g. 'main' results in 'main.bundle.js'.
- * <path>: Defines the path to an entry file. Entry files can use require.context()
- * to search a directory and include multiple files matching a pattern.
- */
-const entryFiles = [{
-  id: 'components',
-  path: path.resolve(__dirname, '../bundles/components.js')
-}];
 
 // Create separate entry points
 const entries = {};
-entryFiles.forEach(function (fileData) {
+CONFIG.aem.entries.forEach(function (fileData) {
   entries[fileData.id] = fileData.path;
 });
 
@@ -77,7 +64,7 @@ module.exports = {
           loader: 'sass-loader',
           options: {
             includePaths: [
-              path.resolve(__dirname, '../../content/jcr_root/apps/__appsFolderName__/components/webpack.resolve/'),
+              CONFIG.aem.jcrRoot + '/apps/' + CONFIG.aem.projectFolderName + '/components/webpack.resolve/'
             ],
           },
         }]
@@ -86,17 +73,16 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    library: libraryName,
-    path: outputPath
+    library: CONFIG.aem.componentLibraryName,
+    path: CONFIG.aem.jcrRoot + '/apps/' + CONFIG.aem.projectFolderName + '/clientlibs/webpack.bundles',
   },
   plugins: [
     extractCSS,
   ],
-  // If your node_modules folder is not in a parent folder of all source files, webpack cannot find the loader. That's shy we have to set an absolute path using the resolveLoader.root option.
   resolve: {
     extensions: ['.js', '.scss'],
     modules: [
-      path.resolve(__dirname, '../../content/jcr_root/apps/__appsFolderName__/components/webpack.resolve/'),
+      CONFIG.aem.jcrRoot + '/apps/' + CONFIG.aem.projectFolderName + '/components/webpack.resolve/',
       NODE_MODULES
     ]
   },
